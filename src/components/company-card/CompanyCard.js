e547f2b9726a0400061a1b867bf7b948e59d723b
victor vinci <victorvinci@protonmail.com>
2019-07-04 15:36:45 +0100

    add more data to be displayed

--- src/components/company-card/CompanyCard.js
+++ src/components/company-card/CompanyCard.js
@@ -5,9 +5,13 @@ class CompanyCard  extends Component {
   
   render() { 
     return (
-      <li> 
-        {this.props.company.name}
+      <li>
+        <img src={`${this.props.company.logo}`} alt="logo"></img>
+        <h2>{this.props.company.name}, founded in {this.props.company.yearFounded}</h2>
+        <p>{this.props.company.description}</p>
+        <p>{this.props.company.sectors}</p>
       </li>
+
      );
   }
 }
