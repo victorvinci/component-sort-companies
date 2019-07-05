b2cfc1d7ef2e8ea0540b3e2b04432f538695233d
victor vinci <victorvinci@protonmail.com>
2019-07-05 13:36:15 +0100

    style the sector content

--- src/components/company-card/CompanyCard.js
+++ src/components/company-card/CompanyCard.js
@@ -9,7 +9,9 @@ class CompanyCard  extends Component {
         <img src={`${this.props.company.logo}`} alt="logo"></img>
         <h2>{this.props.company.name}, founded in {this.props.company.yearFounded}</h2>
         <p>{this.props.company.description}</p>
-        <p>{this.props.company.sectors}</p>
+        <aside>{this.props.company.sectors.map(sector => {
+          return <p>{sector}</p>
+        })}</aside>
       </li>
 
      );
