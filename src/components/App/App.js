a83e11ded6dd472ef30601ef99a2ca51255a1302
victor vinci <victorvinci@protonmail.com>
2019-07-05 20:07:39 +0100

    comment import logo statement

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logos/logo.svg';
+// import logo from '../../assets/logos/logo.svg';
 import './App.scss';
 
 import ListCompanies from '../list-companies/ListCompanies'
